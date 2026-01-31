f96de3c4446495934fa00192f66f2a16c163a41f
Manvanth M <4mh23cs083@gmail.com>
2026-01-31 13:10:19 +0530

    CNN

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from os import name
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -17,4 +17,5 @@ def abc():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
