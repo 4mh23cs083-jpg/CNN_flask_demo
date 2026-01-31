5c93ca6c0e6ddc5b453f356496078d0000eacc48
Manvanth M <4mh23cs083@gmail.com>
2026-01-31 15:50:06 +0530

    Flask

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route("/")
 def home():
